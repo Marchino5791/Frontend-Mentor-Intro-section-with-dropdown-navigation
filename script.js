666592a10b9037f1d694e8d2f9d40259f45b436e
Marco Paolino <marcoantonio.paolino@gmail.com>
2023-02-13 17:28:06 +0100

    Work complete

---
+++ script.js
@@ -0,0 +1,28 @@
+
+$(".open").click(function() {
+    $(".menu").addClass("show-menu");
+    $(".overlay-effect").addClass("show-overlay");
+})
+
+$(".menu__close").click(function () {
+    $(".menu").removeClass("show-menu");
+    $(".overlay-effect").removeClass("show-overlay");
+})
+
+$("#features-link").click(function () {
+    $(".features").slideToggle(300);
+    $(".arrow").toggleClass("arrow-up");
+})
+
+$("#company-link").click(function () {
+    $(".company").slideToggle(300);
+    $(".arrow").toggleClass("arrow-up");
+})
+
+
+
+// reset when screen is > 500px
+$(window).resize(function () {
+    $(".menu").removeClass("show-menu");
+    $(".overlay-effect").removeClass("show-overlay");
+});
